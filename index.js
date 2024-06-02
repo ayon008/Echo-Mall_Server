f8fb1ac75d71548147970f4332501c60dcdccdac
ayon008 <shariar.ayon128@gmail.com>
2024-06-02 12:27:01 +0600

    first commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const uri = `mongodb+srv://${process.env.USER}:${process.env.USER_PASS}@cluster0.sistehv.mongodb.net/?retryWrites=true&w=majority`;
+const uri = "mongodb+srv://ayon008:shariar5175@cluster0.km1azrr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -20,21 +20,22 @@ const client = new MongoClient(uri, {
   },
 });
 
+// FlashSells 
+// Category
+// All products
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-    const userCollection = client.db("dreamgalaxyshop2").collection("users");
-    const productDataCollection = client
-      .db("dreamgalaxyshop2")
-      .collection("ProductData");
-    const ProductPurchaseListCollection = client
-      .db("dreamgalaxyshop2")
-      .collection("ProductPurchaseList");
-    const ProductAddToCollection = client
-      .db("dreamgalaxyshop2")
-      .collection("ProductAddToCart");
+    const dataBase = client.db("Ecommarce")
+    const userCollection = dataBase.collection("users");
+    const productDataCollection = dataBase.collection("productData");
+    const ProductPurchaseListCollection = dataBase.collection("ProductPurchaseList");
+    const ProductAddToCollection = dataBase.collection("ProductAddToCart");
+    const categoriesCollection = dataBase.collection("categories");
 
     // Save User Email And Role In DB
     app.put("/users/:email", async (req, res) => {
@@ -72,12 +73,10 @@ async function run() {
 
     // Get Single Id
     app.get("/data/:_id", async (req, res) => {
-      const _id = req.params.id;
-      const query = {
-        _id: new ObjectId(_id),
-      };
-      const user = await productDataCollection.findOne(query);
-      res.send(user);
+      const id = req.params._id;
+      const query = { _id: id }
+      const findOne = await productDataCollection.findOne(query);
+      res.send(findOne)
     });
 
     // Post Product purchase Data
@@ -90,19 +89,75 @@ async function run() {
     // Post Product Add To cart
     app.post("/ProductAddToCart", async (req, res) => {
       const newItem = req.body;
+      const id = newItem._id;
+      const find = await ProductAddToCollection.findOne({ _id: id });
+      if (find) {
+        const options = { upsert: true };
+        const updateDoc = {
+          $set: {
+            quantity: parseInt(find.quantity) + 1
+          }
+        }
+        const result = await ProductAddToCollection.updateOne({ _id: id }, updateDoc, options)
+        return res.send(result);
+      }
+      newItem.quantity = 1;
       const result = await ProductAddToCollection.insertOne(newItem);
       res.send(result);
     });
+
     // Get Product Add To cart
     app.get("/ProductAddToCart", async (req, res) => {
-      const result = await ProductAddToCollection.find().toArray();
+      const email = req.query.email;
+      const query = { email: { $eq: email } }
+      const result = await ProductAddToCollection.find(query).toArray();
       res.send(result);
     });
 
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({
-    //     ping: 1
-    // });
+    // Delete Product Add To Cart
+    app.delete("/ProductAddToCart/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: id }
+      const result = await ProductAddToCollection.deleteOne(query);
+      res.send(result)
+    });
+
+    // Update Cart Quantity;
+
+    app.patch('/ProductAddToCart/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: id };
+      const { quantity } = req.body;
+
+      const updateDoc = {
+        $set: {
+          quantity: quantity
+        }
+      }
+      const result = await ProductAddToCollection.updateOne(query, updateDoc, { upsert: true });
+      res.send(result);
+    })
+
+    // Delete Cart
+    app.delete('/ProductAddToCart', async (req, res) => {
+      const result = await ProductAddToCollection.deleteMany({})
+      res.send(result);
+    })
+
+    // Get all the categories || TO DO
+    app.get('/categories', async (req, res) => {
+      const categories = await categoriesCollection.find().toArray();
+      res.send(categories);
+    })
+
+    // Get Product by category
+    app.get('/products/:category', async (req, res) => {
+      const category = req.params.category;
+      const query = { category: { $eq: category } }
+      const result = await productDataCollection.find(query).toArray();
+      res.send(result);
+    })
+
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
@@ -120,3 +175,4 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`running ${port}`);
 });
+
