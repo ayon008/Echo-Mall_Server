4fc8b69e9c84efcf9870395659979ebd72a9341d
ayon008 <shariar.ayon128@gmail.com>
2024-06-04 10:33:58 +0600

    80%

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("dotenv").config();
+var jwt = require('jsonwebtoken');
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
@@ -9,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const uri = "mongodb+srv://ayon008:shariar5175@cluster0.km1azrr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.USER}:${process.env.USER_PASS}@cluster0.km1azrr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,17 +26,74 @@ const client = new MongoClient(uri, {
 // All products
 
 
+const verifyToken = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'Invalid authorization' });
+  }
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, (error, decoded) => {
+    if (error) {
+      return res.status(401).send({ error: true, message: 'Authorization Failed' });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
+
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-
-    const dataBase = client.db("Ecommarce")
+    const dataBase = client.db("Ecommarce");
     const userCollection = dataBase.collection("users");
     const productDataCollection = dataBase.collection("productData");
     const ProductPurchaseListCollection = dataBase.collection("ProductPurchaseList");
     const ProductAddToCollection = dataBase.collection("ProductAddToCart");
     const categoriesCollection = dataBase.collection("categories");
+
+    // Post User
+    app.post('/user', async (req, res) => {
+      const data = req.body;
+      const query = { email: { $eq: data.email } }
+      const find = await userCollection.findOne(query);
+      if (find) {
+        return
+      }
+      const result = await userCollection.insertOne(data);
+      res.send(result);
+    })
+
+    // Patch User
+    app.patch('/user/:uid', verifyToken, async (req, res) => {
+      const id = req.params.uid;
+      const data = req.body;
+      const query = { uid: { $eq: id } };
+      const options = { upsert: true }
+      const updateDoc = {
+        $set: {
+          name: data.name,
+          address: data.address,
+          mobileNumber: data.mobileNumber
+        }
+      }
+      const result = await userCollection.updateOne(query, updateDoc, options)
+      res.send(result);
+    })
+
+    // get user 
+    app.get('/user/:uid', verifyToken, async (req, res) => {
+      const uid = req.params.uid;
+      const query = { uid: { $eq: uid } };
+      const result = await userCollection.findOne(query);
+      res.send(result);
+    })
+
+    // jwt token
+    app.post('/jwt', async (req, res) => {
+      const email = req.body.email;
+      const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1hr' })
+      res.send({ token });
+    })
 
     // Save User Email And Role In DB
     app.put("/users/:email", async (req, res) => {
@@ -51,12 +109,13 @@ async function run() {
     });
 
     // Own Data Find
-    app.get("/users/:email", async (req, res) => {
+    app.get("/users/:email", verifyToken, async (req, res) => {
       const result = await userCollection
         .find({ email: req.params.email })
         .toArray();
       res.send(result);
     });
+
 
     // Post Product Data
     app.post("/data", async (req, res) => {
@@ -107,7 +166,7 @@ async function run() {
     });
 
     // Get Product Add To cart
-    app.get("/ProductAddToCart", async (req, res) => {
+    app.get("/ProductAddToCart", verifyToken, async (req, res) => {
       const email = req.query.email;
       const query = { email: { $eq: email } }
       const result = await ProductAddToCollection.find(query).toArray();
@@ -124,7 +183,7 @@ async function run() {
 
     // Update Cart Quantity;
 
-    app.patch('/ProductAddToCart/:id', async (req, res) => {
+    app.patch('/ProductAddToCart/:id', verifyToken, async (req, res) => {
       const id = req.params.id;
       const query = { _id: id };
       const { quantity } = req.body;
@@ -139,7 +198,7 @@ async function run() {
     })
 
     // Delete Cart
-    app.delete('/ProductAddToCart', async (req, res) => {
+    app.delete('/ProductAddToCart', verifyToken, async (req, res) => {
       const result = await ProductAddToCollection.deleteMany({})
       res.send(result);
     })
@@ -157,6 +216,15 @@ async function run() {
       const result = await productDataCollection.find(query).toArray();
       res.send(result);
     })
+
+
+    app.get('/suggestions', async (req, res) => {
+      const search = req.query.search;
+      const query = { Product_Name: { $regex: search, $options: 'i' } }
+      const find = await productDataCollection.find(query).toArray();
+      res.send(find);
+    });
+
 
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
